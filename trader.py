4624e564b89115822d1ba8a72b097cb323262a57
Ben Thomas <BenThomas@Bens-MacBook-Pro.local>
2019-05-06 15:16:26 +0100

    updated for environment variables and optional use of sandbox

--- trader.py
+++ trader.py
@@ -1,5 +1,12 @@
 from kucoin.client import Client
+from decouple import config
 
 class Trader:
-    def __init__(self, api_key, api_secret, api_passphrase):
-        self.client = Client(api_key, api_secret, api_passphrase)
+    def __init__(self, use_sandbox):
+        if(use_sandbox is True):
+            self.client = Client( config('SAND_PUBLIC'), config('SAND_SECRET'), config('PASS_CODE'), sandbox=True )
+        else:
+            self.client = Client( config('PUBLIC_KEY'), config('SECRET_KEY'), config('PASS_CODE') )
+
+    def get_market_list(self):
+        return self.client.get_market_list()
