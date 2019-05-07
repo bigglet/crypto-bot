551defde7c71e7353dbbb6448f33fad7ffbf8b6e
Ben Thomas <BenThomas@Bens-MacBook-Pro.local>
2019-05-07 11:24:57 +0100

    fixed passphrase issue and got list of trading pairs

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from trader import Trader
 
-
-use_sandbox=True
+use_sandbox=False
 
 def run():
     trader = initialise_trader()
-    print(trader.get_market_list())
+    trader.sell_all_currencies()
+    current_coin = 'BTC'
+    #while(1):
+    #    currency_symbols = trader.get_symbol_list()
+    #    update_lists()
+    #    check_if_enough_data()
+    #    make trade()
+    #    update_coin()
 
 def initialise_trader():
     return Trader(use_sandbox)
--- trader.py
+++ trader.py
@@ -4,9 +4,21 @@ from decouple import config
 class Trader:
     def __init__(self, use_sandbox):
         if(use_sandbox is True):
-            self.client = Client( config('SAND_PUBLIC'), config('SAND_SECRET'), config('PASS_CODE'), sandbox=True )
+            self.client = Client( config('SAND_PUBLIC'), config('SAND_SECRET'), config('PASS_PHRASE'), sandbox=True )
         else:
-            self.client = Client( config('PUBLIC_KEY'), config('SECRET_KEY'), config('PASS_CODE') )
+            self.client = Client( config('PUBLIC_KEY'), config('SECRET_KEY'), config('PASS_PHRASE') )
 
-    def get_market_list(self):
-        return self.client.get_market_list()
+    def sell_all_currencies(self):
+        print( self.client.get_accounts() )
+
+    def get_symbol_list(self, sell_currency=None):
+        currency_symbol_list = []
+        if(sell_currency is None):
+            for currency_stats in self.client.get_symbols():
+                currency_symbol_list.append( currency_stats["symbol"] )
+        else:
+            for currency_stats in self.client.get_symbols():
+                if( currency_stats["symbol"].endswith('BTC') ):
+                    currency_symbol_list.append( currency_stats["symbol"] )
+
+        return currency_symbol_list
