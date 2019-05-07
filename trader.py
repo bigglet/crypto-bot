c82bc7e534caa65d3c569f805d3209787271fb02
Ben Thomas <BenThomas@Bens-MacBook-Pro.local>
2019-05-07 18:35:25 +0100

    Sell all currencies to BTC when bot starts

--- trader.py
+++ trader.py
@@ -9,16 +9,24 @@ class Trader:
             self.client = Client( config('PUBLIC_KEY'), config('SECRET_KEY'), config('PASS_PHRASE') )
 
     def sell_all_currencies(self):
-        print( self.client.get_accounts() )
+        for coins_in_wallet in self.client.get_accounts():
+            if(coins_in_wallet["currency"] != "BTC"):
+                print( coins_in_wallet["currency"] + ":\t" + coins_in_wallet["balance"] + "\t/\t" + coins_in_wallet["available"] )
+                sell_pair = coins_in_wallet["currency"] + "-BTC"
+                print( "Create Sell order for: " + sell_pair )
+                try:
+                    self.client.create_market_order(sell_pair, 'sell', coins_in_wallet["available"])
+                except:
+                    print("DID NOT SELL")
 
-    def get_symbol_list(self, sell_currency=None):
+    def get_symbol_list(self, quote_currency=None):
         currency_symbol_list = []
-        if(sell_currency is None):
+        if(quote_currency is None):
             for currency_stats in self.client.get_symbols():
                 currency_symbol_list.append( currency_stats["symbol"] )
         else:
             for currency_stats in self.client.get_symbols():
-                if( currency_stats["symbol"].endswith('BTC') ):
+                if( currency_stats["symbol"].endswith(quote_currency) ):
                     currency_symbol_list.append( currency_stats["symbol"] )
 
         return currency_symbol_list
